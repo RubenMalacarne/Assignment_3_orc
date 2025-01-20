b41e2df2e4b3eaff0a7ac4f1a7ef68ab913f943a
Ruben <ruben.malacarne@gmail.com>
2025-01-20 15:01:36 +0100

    add some value in config file

--- Double_pendulum/conf_double_pendulum.py
+++ Double_pendulum/conf_double_pendulum.py
@@ -1,57 +1,63 @@
 # -*- coding: utf-8 -*-
 """
-Configuration file to Double_Pendulum
+Configuration file for Double Pendulum
 """
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from time import time as clock, sleep
 
-from time import time as clock
-from time import sleep
-
+# Start time
 time_start = clock()
-SIMULATOR = "ideal" #switch btw "pinocchio" or "ideal"
-DO_PLOTS = False
+# General Configuration
+# Goal point
+q_des = np.array([0,0])  # Default: "pendolo a piombo" --> np.array([-np.pi, -np.pi*2])
 
-# General_Configuration
-    # goal point
-# q_des = np.array([0, 0]) 
-q_des =np.array([np.pi, np.pi])
-    # weight factor 
-w_p = 1e2       # position weight
-w_v = 1e-1      # velocity weight 
-w_a = 1e-6      # acceleration weight
-w_final = 1e2   # final cost weight --> not used
+# Horizon parameters
+dt = 0.01     # OCP time step
+N_sim = 100    # Simulation steps
+N_step = 100  # Time horizon steps
+M_step = 30
+max_iter_opts = 1000
+SCALE = 10000
 
-    # number of the initial states #non gli piacciono i numeri dispari
-n_init_state_ocp = 11
+# Initial configuration
+q0 = np.array([-np.pi, -np.pi * 2])
+# length of link 1 and 2
+L1 = 0.035  
+L2 = 0.1    
+# Constraint parameters
+# These limits are not present in the URDF file
+TAU_MAX = 1
+TAU_MIN = -1
+# CSV file saving
 
-    # parameter for visualization 
+# Initial states
+n_init_state_ocp = 11  # Avoid odd numbers if disliked
+
+# Weight factors
+w_p = 1e2   # position weight
+w_v = 0e-6  # velocity weight
+w_a = 1e-5   # Acceleration weight
+w_final = 1e1   # Final cost weight (not used)
+
+# Visualization parameters  
+# Simulator settings
 np.set_printoptions(precision=2, linewidth=200, suppress=True)
 LINE_WIDTH = 1
-SIMULATOR = "ideal" #"mujoco" or "pinocchio" or "ideal"
+SIMULATOR = "ideal"  # Options: "mujoco", "pinocchio", "ideal"
+DO_PLOTS = False
 nq = 2
 
-    # simulation parameter  
-simulate_coulomb_friction = 0    # flag specifying whether coulomb friction is simulated
-simulation_type = 'euler' # either 'timestepping' or 'euler'
-tau_coulomb_max = 0*np.ones(nq)   # expressed as percentage of torque max
+
+# Simulation parameters
+simulate_coulomb_friction = 0  # Flag for simulating Coulomb friction
+simulation_type = 'euler'      # Options: 'timestepping', 'euler'
+tau_coulomb_max = 0 * np.ones(nq)  # Max Coulomb torque as percentage
 randomize_robot_model = 0
 use_viewer = True
 which_viewer = 'meshcat'
-simulate_real_time = 1          # flag specifying whether simulation should be real time or as fast as possible
+simulate_real_time = 1         # Real-time simulation flag
 show_floor = False
-DISPLAY_T = 0.02              # update robot configuration in viwewer every DISPLAY_T seconds
-    # constraint parameter  because in the URDF file it's not present this limits
-TAU_MAX = 0.1
-TAU_MIN = -0.1
-    # how to save csv file
-booltrain_file = False # TRUE: ocp_dataset_DP_train  --- FALSE: ocp_dataset_DP_eval
+DISPLAY_T = 0.02               # Viewer update interval (seconds)
 
-    # horizon parameters--> N
-dt = 0.01        # OCP time step+
-dt_sim = 0.002
-#time horizon
-N_step =10
-M_step =1
-max_iter_opts = 500
-SCALE = 10000
+
